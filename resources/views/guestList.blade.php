3ce9b55d930ead9594be7941c481b2863c76179d
Randi Adiel Gianufian <randiadiel30@gmail.com>
2021-07-27 15:57:23 +0700

    Add responsiveness to the website

--- resources/views/guestList.blade.php
+++ resources/views/guestList.blade.php
@@ -18,12 +18,11 @@
                 <div class="font-button-text text-biru1"><a href="{{ route('home') }}">Home</a></div>
                 <div class="font-button-selected text-kuning2"><a href="{{ route('guest') }}">guests</a></div>
             </div>
-
         </nav>
         <img class="guestList-women-graphic" src="{{ asset('assets/images/women2.png') }}"
             alt="3D Women Sitting with Laptop">
         <div class="guestList-content">
-            <div>
+            <div class="guestList-content-wrapper">
                 <h1 class="font-heading1 text-biru1">All <span class="text-kuning2">guest</span> list</h1>
                 <div class="guestList-table">
                     <table>
